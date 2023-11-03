7579d654dddd3052e1de17740c3be7cd38f6df12
Shivam <70581178+shivamkonkar@users.noreply.github.com>
2023-11-03 13:50:53 +0530

    Initial commit

---
+++ src/main/java/ninja/shivamkonkar/MoviesAPI/MovieService.java
@@ -0,0 +1,20 @@
+package ninja.shivamkonkar.MoviesAPI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private  MovieRepository movieRepository;
+    public List<Movie> getAllMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId){ return movieRepository.findMovieByImdbId(imdbId);}
+
+}
